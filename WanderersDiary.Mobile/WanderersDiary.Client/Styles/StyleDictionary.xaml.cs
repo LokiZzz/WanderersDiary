753869dc5351925853d7be73d5ff3120ef82aa10
LokiZzZ <lokizzzzzzzz@gmail.com>
2021-11-07 21:52:54 +0300

    Styles;

---
+++ WanderersDiary.Mobile/WanderersDiary.Client/Styles/StyleDictionary.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WanderersDiary.Client.Styles
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StyleDictionary : ResourceDictionary
+    {
+        public StyleDictionary()
+        {
+            InitializeComponent();
+        }
+    }
+}
